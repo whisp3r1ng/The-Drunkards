02ad084ebf43cef42e54f175465c71330e25f2eb
weihao94 <khoongweihao@hotmail.com>
2016-06-26 23:02:12 +0800

    Add files via upload

--- Test Files/The Drunkards ver 1.0.py
+++ Test Files/The Drunkards ver 1.0.py
@@ -9,12 +9,12 @@ from google.appengine.ext import ndb
 
 
 jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/Templates"))
 
 class MainPage(webapp2.RequestHandler): #Handler for the main page
 
     def get(self):
-        template = jinja_environment.get_template('front.html')
+        template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render())
 
 class MainPageUser(webapp2.RequestHandler): #Front page for those logged in
@@ -26,7 +26,7 @@ class MainPageUser(webapp2.RequestHandler): #Front page for those logged in
                 'user_nickname': users.get_current_user().nickname(),
                 'logout': users.create_logout_url(self.request.host_url),
             }
-            template = jinja_environment.get_template('front.html')
+            template = jinja_environment.get_template('index.html')
             self.response.out.write(template.render(template_values))
         else:
             self.redirect(self.request.host_url)
