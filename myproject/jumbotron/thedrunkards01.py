2056abb155721c9a51173f62db96980f14234293
weihao94 <khoongweihao@hotmail.com>
2016-07-04 18:30:13 +0800

    Add files via upload

--- myproject/jumbotron/thedrunkards01.py
+++ myproject/jumbotron/thedrunkards01.py
@@ -14,8 +14,17 @@ jinja_environment = jinja2.Environment(
 class MainPage(webapp2.RequestHandler): #Handler for the main page
 
     def get(self):
-        template = jinja_environment.get_template('index.html')
-        self.response.out.write(template.render())
+        user = users.get_current_user()
+        if user:  # signed in already
+            template_values = {
+                'user_nickname': users.get_current_user().nickname(),
+                'logout': users.create_logout_url(self.request.host_url),
+                }
+            template = jinja_environment.get_template('index.html')
+            self.response.out.write(template.render(template_values))
+        else:
+            template = jinja_environment.get_template('index.html')
+            self.response.out.write(template.render())
 
 class About(webapp2.RequestHandler): #Handler for the about page
 
@@ -28,19 +37,68 @@ class stall1(webapp2.RequestHandler): #Handler for the stores
         template = jinja_environment.get_template('stall1.html')
         self.response.out.write(template.render())
 
+class stall1_page(webapp2.RequestHandler): #Handler for the stores
+
+    def get(self):
+        user = users.get_current_user()
+        if user:  # signed in already
+            template_values = {
+                'user_nickname': users.get_current_user().nickname(),
+                'logout': users.create_logout_url(self.request.host_url),
+                }
+            template = jinja_environment.get_template('stall1_page.html')
+            self.response.out.write(template.render(template_values))
+        else:
+            template = jinja_environment.get_template('stall1_page.html')
+            self.response.out.write(template.render())
+
 class stall2(webapp2.RequestHandler): #Handler for the stores
     def get(self):
         template = jinja_environment.get_template('stall2.html')
         self.response.out.write(template.render())
+
+class stall2_page(webapp2.RequestHandler): #Handler for the stores
+
+    def get(self):
+        user = users.get_current_user()
+        if user:  # signed in already
+            template_values = {
+                'user_nickname': users.get_current_user().nickname(),
+                'logout': users.create_logout_url(self.request.host_url),
+                }
+            template = jinja_environment.get_template('stall2_page.html')
+            self.response.out.write(template.render(template_values))
+        else:
+            template = jinja_environment.get_template('stall2_page.html')
+            self.response.out.write(template.render())
 
 class stall3(webapp2.RequestHandler): #Handler for the stores
     def get(self):
         template = jinja_environment.get_template('stall3.html')
         self.response.out.write(template.render())
 
+class stall3_page(webapp2.RequestHandler): #Handler for the stores
+
+    def get(self):
+        user = users.get_current_user()
+        if user:  # signed in already
+            template_values = {
+                'user_nickname': users.get_current_user().nickname(),
+                'logout': users.create_logout_url(self.request.host_url),
+                }
+            template = jinja_environment.get_template('stall3_page.html')
+            self.response.out.write(template.render(template_values))
+        else:
+            template = jinja_environment.get_template('stall3_page.html')
+            self.response.out.write(template.render())
+
 app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/thedrunkards', MainPage),
                                ('/about', About),
                                ('/stall1', stall1),
+                               ('/stall1_page', stall1_page),
                                ('/stall2', stall2),
-                               ('/stall3', stall3)],
+                               ('/stall2_page', stall2_page),
+                               ('/stall3', stall3),
+                               ('/stall3_page', stall3_page)],
                                debug=True)
